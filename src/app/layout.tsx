cdd72c7ac7346e583c41d2ea4587b3a89ad1cae8
AaryanKh <66236478+AaryanKhClasses@users.noreply.github.com>
2025-02-11 18:33:31 +0530

    tuner and fixed random margin in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
                 <Provider>
                     <Nav />
-                    <div className="container my-6 px-3 flex flex-col min-h-screen">
+                    <div className="my-6 px-3 flex flex-col min-h-screen">
                         <div className="flex-grow">{children}</div>
                         {/* <footer className="py-4 bg-gray-800 text-center text-sm text-gray-400">
                             <p>&copy; 2025 SangeetAI. All rights reserved.</p>
--- src/components/Tuner.tsx
+++ src/components/Tuner.tsx
@@ -1,5 +1,82 @@
 "use client"
 
+import { Button } from "@heroui/button"
+import { Input } from "@heroui/input"
+import { PitchDetector } from "pitchy"
+import { useCallback, useEffect, useState } from "react"
+
+const audioContext = new window.AudioContext()
+const analyserNode = audioContext.createAnalyser()
+
+const noteFrequencies = [
+    { note: "Sa (C)", frequency: 261.63 },
+    { note: "Re (D)", frequency: 293.66 },
+    { note: "Ga (E)", frequency: 329.63 },
+    { note: "Ma (F)", frequency: 349.23 },
+    { note: "Pa (G)", frequency: 392.00 },
+    { note: "Dha (A)", frequency: 440.00 },
+    { note: "Ni (B)", frequency: 493.88 }
+]
+
+function getNearestNote(pitch: number) {
+    let minDiff = Infinity
+    let nearestNote = ""
+    for (const { note, frequency } of noteFrequencies) {
+        const diff = Math.abs(pitch - frequency)
+        if (diff < minDiff) {
+            minDiff = diff
+            nearestNote = note
+        }
+    }
+    return nearestNote
+}
+
 export function Tuner() {
-    return <></>
+    const [pitch, setPitch] = useState(0)
+    const [clarity, setClarity] = useState(0)
+    const [note, setNote] = useState("")
+    useEffect(() => {
+        let mediaStream: MediaStream
+        navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+            mediaStream = stream
+            audioContext.createMediaStreamSource(stream).connect(analyserNode)
+            const detector = PitchDetector.forFloat32Array(analyserNode.fftSize)
+            detector.minVolumeDecibels = -100
+            const input = new Float32Array(analyserNode.fftSize)
+            const update = () => {
+                analyserNode.getFloatTimeDomainData(input)
+                const [outPitch, clarity] = detector.findPitch(input, audioContext.sampleRate)
+                setPitch(Math.round(outPitch * 10) / 10)
+                setClarity(Math.round(clarity * 100) / 100)
+                setNote(getNearestNote(outPitch))
+                requestAnimationFrame(update)
+            }
+            update()
+        }).catch(error => {
+            console.error("Error accessing microphone: ", error)
+        })
+        return () => {
+            if (mediaStream) {
+                mediaStream.getTracks().forEach(track => track.stop())
+            }
+            audioContext.close()
+        }
+    }, [])
+    const handleButtonClick = useCallback(() => {
+        audioContext.state === "running" ? audioContext.suspend() : audioContext.resume()
+    }, [])
+    return <div className="container mx-auto p-4">
+        <div className="mb-4">
+            <h2 className="text-2xl">Pitch: <Input disabled value={pitch.toString()} /></h2>
+        </div>
+        <div className="mb-4">
+            <h2 className="text-2xl">Clarity: <Input disabled value={clarity.toString()} /></h2>
+        </div>
+        <div className="mb-4">
+            <h2 className="text-2xl align-center justify-self-center">{note}</h2>
+        </div>
+        <Button onPress={handleButtonClick} className="flex items-center">
+            {audioContext.state === "running" ? <><i className="bi bi-stop-fill mr-2"></i><span>Stop</span></> : <><i className="bi bi-play-fill mr-2"></i><span>Start</span></>}
+        </Button>
+    </div>
 }
